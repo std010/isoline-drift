6807289cd23fce88769ab6d89a40a70220949772
std010 <gao24lm030@student.bmstu.ru>
2025-12-19 00:12:51 +0300

    important data in separate subplots

--- prog0.m
+++ prog0.m
@@ -5,7 +5,12 @@ E=load('ecg_original_with_rpeaks.txt');
 
 figure('Name','ECG','NumberTitle','off');
 
-plot(E(:,:));
+subplot(2,1,1)
+plot(E(:,2))
 title('ECG Signal')
-
+ylim([1900 2600])
  
+subplot(2,1,2)
+plot(E(:,3))
+title('ECG RR')
+ylim([-5 5])
